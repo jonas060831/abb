ee5aa3edbffc0e6f7de5284438f8979df60aad70
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-06-21 14:12:26 -0700

    use the total headcount on each rsvp instead of just using each rsvp

--- ui/ConfirmedHeadCount/ConfirmedHeadCount.tsx
+++ ui/ConfirmedHeadCount/ConfirmedHeadCount.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { getAllRsvp } from '@/app/(serverFunctions)/rsvp'
-import React, { useEffect, useState } from 'react'
+import { getAllRsvp, RsvpEntry } from '@/app/(serverFunctions)/rsvp'
+import React, { useEffect, useMemo, useState } from 'react'
 
 const ConfirmedHeadCount = () => {
 
-  const [rsvp, setRsvp] = useState<number | null>(null)
+  const [rsvp, setRsvp] =  useState<RsvpEntry[]>([]);
 
   useEffect(() => {
       
@@ -16,25 +16,30 @@ const ConfirmedHeadCount = () => {
     const res = await getAllRsvp();
 
     if (res && Array.isArray(res.data)) {
-      setRsvp(res.data.length);
+      //setRsvp(res.data.length);
+      setRsvp(res.data)
     } else {
       console.warn("No RSVPs found or invalid response format.");
-      setRsvp(0);
+      setRsvp([]);
     }
   } catch (error) {
     console.error("Failed to fetch RSVPs:", error);
-    setRsvp(0);
+    setRsvp([]);
   }
 };
 
+const totalHeadcount = useMemo(() => {
+    return rsvp.reduce((total, guest) => total + guest.guestNames.length, 0);
+  }, [rsvp]);
 
-  if(rsvp === null && rsvp !== 0) return <p style={{ position: 'absolute', display: 'flex', alignSelf: 'center', justifySelf: 'center', marginTop: '-36rem' }}>Loading...</p>
+
+  if(rsvp.length === 0) return <p style={{ position: 'absolute', display: 'flex', alignSelf: 'center', justifySelf: 'center', marginTop: '-36rem' }}><img src='/medias/svgs/loading-spinner-black.svg' alt='' style={{ width: '25px' }}/></p>
   
   //because 0 also evaluate to false
-  if(rsvp === 0) return null
+  // if(rsvp.length === 0) return null
 
   return (
-    <p style={{ position: 'absolute', display: 'flex', alignSelf: 'center', justifySelf: 'center', marginTop: '-36rem' }}>{rsvp} Confirmed</p>
+    <p style={{ position: 'absolute', display: 'flex', alignSelf: 'center', justifySelf: 'center', marginTop: '-36rem', fontFamily: 'var(--font-raleway)' }}>{totalHeadcount} Confirmed</p>
   )
 }
 
